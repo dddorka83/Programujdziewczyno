4a3d1c49fc7041ddb8310b5a5009627639b51ab7
dddorka83 <dorota.drozd@gmail.com>
2017-10-21 16:19:15 +0200

    zadanie 2 - rozwijanie i zwijanie przycisku

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
   
+    //zadanie 1
     var menuElement = document.querySelector('.for-dropdown');
     var dropdownList = document.querySelector('.for-dropdown .dropdown');
     
@@ -11,6 +12,30 @@ document.addEventListener('DOMContentLoaded', function() {
     menuElement.addEventListener('mouseout', function() {
        dropdownList.style.display = 'none';
     });
+    
+    //zadanie 2
+    var buttons = document.querySelectorAll('.read-more');
+   
+    function showHide() {
+        var textArea = this.previousElementSibling;
+        
+        if (textArea.style.display === 'none' || textArea.style.display === ''){
+        textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        }  else {
+        textArea.style.display = 'none';
+        this.innerHTML = 'Więcej <span class="glyphicon glyphicon-chevron-down"></span>';
+        }  
+          
+    }
+    // buttons[0].addEventListener('click', showHide);
+    
+     for (var i = 0; i <= buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
+    
+    
+     
     
 });
 
